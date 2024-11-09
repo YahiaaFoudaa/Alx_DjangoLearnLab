13f1176f61bc0bcd271b3ffa8d574c0fc7dd6cf4
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-10 00:06:10 +0200

    DM2

--- django-models/IAM/settings.py
+++ django-models/IAM/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'relationship_app.apps.RelationshipAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
