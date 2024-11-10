1f97ee23b733d783fa44906cbea7ea9478de294d
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-10 20:53:30 +0200

    t7

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -25,4 +25,4 @@ class LibraryDetailView(DetailView):
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'relationship_app/register.html'
