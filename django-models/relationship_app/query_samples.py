12b44bfd8a1eeafe65d36329c013bdd746b591d0
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-10 20:18:13 +0200

    marwan file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,7 +1,16 @@
-from models import Author,Book,Library,Librarian
+from models import Author, Book , Library, Librarian
 
-author = Author.objects.get(name = "J.K. Rowling")
+def get_books_by_author(author_name,author):
+    Author.objects.get(name=author_name)
+    books = Author.objects.filter(author=author)
+    return books 
 
-books = Book.objects.all()
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all() 
+    return books
 
-librarian = Librarian.objects.get(Library = "mktba")
+def get_librarian_for_library(library_name):
+    Librarian.objects.get(library="")
+    librarian = Library.objects.librarian      
+    return librarian
