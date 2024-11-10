1fd0808af3de604644193f798a227cdddc2940e2
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-10 23:12:57 +0200

    t15

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,8 +6,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.contrib.auth import login
-
-
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
 
 def list_books(request):
     books = Book.objects.all() 
@@ -29,5 +29,27 @@ class register(CreateView):
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
 
+# Check if the user has the 'Admin' role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
+@user_passes_test(is_admin)
+def admin_view(request):
+    # This view is only accessible to users with the 'Admin' role
+    return render(request, 'relationship_app/admin_view.html')  # You can customize the template as needed
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    # This view is only accessible to users with the 'Librarian' role
+    return render(request, 'relationship_app/librarian_view.html')  # Customize the template as needed
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_member)
+def member_view(request):
+    # This view is only accessible to users with the 'Member' role
+    return render(request, 'relationship_app/member_view.html')  # Customize the template as needed
