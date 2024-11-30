b9d9c80d628ec032ce12718d03ab3728e2d63401
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-30 16:33:55 +0200

    aap3

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import BookListView, BookDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics, filters
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class CustomUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class CustomPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['POST', 'PUT', 'DELETE']:
+            return request.user.is_authenticated
+        return True
+
