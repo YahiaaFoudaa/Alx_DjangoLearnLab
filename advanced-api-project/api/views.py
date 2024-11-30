de3004c9ae5ea0159cea631caff26c65924a465f
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-11-30 16:48:54 +0200

    aap4

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,11 +4,11 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticated
 
-class BookListView(generics.ListCreateAPIView):
+class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+class BookDetailView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
