315fc522f541292e2d563e42185600b8ae396072
Yahia Fouda <yahiabdelmonem@gmail.com>
2024-12-13 21:18:26 +0200

    first commit

---
+++ social_media_api/accounts/urls.py
@@ -0,0 +1,11 @@
+# accounts/urls.py
+from django.urls import path
+from .views import UserRegistrationView, CustomAuthToken
+
+urlpatterns = [
+    # URL for user registration
+    path('register/', UserRegistrationView.as_view(), name='user-registration'),
+
+    # URL for login (authentication via token)
+    path('login/', CustomAuthToken.as_view(), name='token-login'),
+]
